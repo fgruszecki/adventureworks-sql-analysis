79a44b3f932f9b5ec2ae9573b507bd90daf7f2c4
fgruszecki <filip.gruszecki1@gmail.com>
2026-07-17 19:51:51 +0200

    Create customer_segmentation.sql

---
+++ customer_segmentation.sql
@@ -0,0 +1,44 @@
+WITH WstepnaSegregacja AS (
+    SELECT SOH.CustomerID,
+
+    
+SOH.OrderDate,
+SOD.LineTotal,
+P.Name,
+SOH.SalesOrderID,
+SOH.SubTotal
+                
+
+    FROM SalesLT.SalesOrderHeader AS SOH
+    JOIN SalesLT.SalesOrderDetail AS SOD ON SOD.SalesOrderID=SOH.SalesOrderID
+    JOIN SalesLT.Product AS P ON P.ProductID=SOD.ProductID
+    WHERE SOH.Status = 5
+),
+Segregacja AS (
+    SELECT CustomerID,
+    COUNT(DISTINCT CASE WHEN SubTotal > 2000 THEN SalesOrderID END) *  100.00 / COUNT(SalesOrderID)[HighValueOrdersPct],
+    SUM(LineTotal)[TotalSpent],
+    MAX(OrderDate)[ZamowienieOstatnie],
+    COUNT(DISTINCT SalesOrderID)[TotalOrdersCount],
+
+        CASE
+            WHEN SUM(CASE WHEN Name LIKE 'BK-%' THEN LineTotal ELSE 0 END) > SUM(CASE WHEN Name NOT LIKE 'BK-%' THEN LineTotal ELSE 0 END ) THEN 'Bikes'
+            ELSE 'Other'
+        END [FavoriteCategory]
+
+    FROM WstepnaSegregacja
+    GROUP BY Customerid
+)
+
+
+SELECT CustomerID,
+    HighValueOrdersPct,
+TotalSpent,
+    YEAR(ZamowienieOstatnie)[LastOrderYear],
+    TotalOrdersCount,
+    FavoriteCategory
+
+    
+FROM Segregacja
+WHERE TotalSpent > 5000
+ORDER BY TotalSpent DESC
